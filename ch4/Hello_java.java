00e74576354fab9dcb0832b8f8f83891413845b6
Minkyoung-Kim121 <79966569+Minkyoung-Kim121@users.noreply.github.com>
2021-05-10 22:59:41 +0900

    Create Hello_java.java

---
+++ ch4/Hello_java.java
@@ -0,0 +1,12 @@
+public class A{
+
+	public A(){
+		System.out.println("A() 생성자 >>> : ");
+	}
+
+	public static void main(String args[]){
+		System.out.println("main() 함수 >>> : ");
+		A a = new A();
+
+	}
+}
